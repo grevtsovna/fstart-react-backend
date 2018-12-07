00d092de935c1b6f5f6e63335012ee331ac64d86
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-12-07 22:26:19 +0700

    Реализован метод для редактирования слов в коллекции

--- packages/collections/collections.js
+++ packages/collections/collections.js
@@ -41,7 +41,6 @@ router.get('/', (req, res) => {
   res.json({ status: 'OK', data: db.has('collections') });
 });
 
-
 router.patch('/:id/name', (req, res, next) => {
   if (typeof req.body.name === 'undefined') next(new Error('INVALID_API_FORMAT'));
   else next();
@@ -49,6 +48,19 @@ router.patch('/:id/name', (req, res, next) => {
   const collection = db.get('collections')
     .find({ id: req.params.id })
     .assign({ name: req.body.name })
+    .value();
+  db.write();
+
+  res.json({ status: 'OK', data: collection });
+});
+
+router.patch('/:id/words', (req, res, next) => {
+  if (typeof req.body.words === 'undefined') next(new Error('INVALID_API_FORMAT'));
+  else next();
+}, (req, res) => {
+  const collection = db.get('collections')
+    .find({ id: req.params.id })
+    .assign({ words: JSON.parse(req.body.words) })
     .value();
   db.write();
 
