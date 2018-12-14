62032a01d16726b71965d42f7a0fcee2c7764518
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-12-15 00:16:55 +0700

    Реализовано изменение слов в коллекции

--- packages/collections/collections.js
+++ packages/collections/collections.js
@@ -15,7 +15,30 @@ router.get('/:id', (req, res) => {
     .find({ id: req.params.id })
     .value();
 
+  const words = collection.words.map((wordId) => {
+    return db.get('words')
+      .find({ id: wordId })
+      .value();
+  });
+
+  collection.words = words;
+
   res.json({ status: 'OK', data: collection });
+});
+
+router.get('/:id/words', (req, res) => {
+  const collection = db
+    .get('collections')
+    .find({ id: req.params.id })
+    .value();
+
+  const words = collection.words.map((wordId) => {
+    return db.get('words')
+      .find(wordId)
+      .value();
+  });
+
+  res.json({ status: 'OK', data: words });
 });
 
 router.post('/', (req, res, next) => {
@@ -55,16 +78,29 @@ router.patch('/:id/name', (req, res, next) => {
 });
 
 router.patch('/:id/words', (req, res, next) => {
-  if (typeof req.body.words === 'undefined') next(new Error('INVALID_API_FORMAT'));
+  if (typeof req.body.word === 'undefined') next(new Error('INVALID_API_FORMAT'));
   else next();
 }, (req, res) => {
   const collection = db.get('collections')
     .find({ id: req.params.id })
-    .assign({ words: JSON.parse(req.body.words) })
     .value();
-  db.write();
 
-  res.json({ status: 'OK', data: collection });
+  collection.words.push(req.body.word);
+
+  // Если тут написать console.log(collection.words) - все работает
+
+  db.get('collections')
+    .find({ id: req.params.id })
+    .assign({ words: collection.words })
+    .write();
+
+  const words = collection.words.map((wordId) => {
+    return db.get('words')
+      .find({ id: wordId })
+      .value();
+  });
+
+  res.json({ status: 'OK', data: words });
 });
 
 router.delete('/:id', (req, res) => {
