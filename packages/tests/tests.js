8b4c40928fc21986b6881df2953a8663367b669b
Никита Гревцов <n.a.grevtsov@gmail.com>
2018-12-15 14:56:18 +0700

    Добавлены ответы для test data

--- packages/tests/tests.js
+++ packages/tests/tests.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const db = require('../db/db');
+const _ = require('lodash');
 
 router.get('/:id', (req, res) => {
   const wordsId = db.get('collections')
@@ -14,12 +15,18 @@ router.get('/:id', (req, res) => {
       .value()
   ));
 
+  const answers = db.get('words')
+    .map('ru')
+    .take(3)
+    .value();
+
   const testData = words.map((word) => ({
     id: word.id,
-    de: word.de
+    de: word.de,
+    answers: _([...answers, word.ru]).shuffle()
   }));
 
-  res.json({status: "OK", data: testData});
+  res.json({status: 'OK', data: testData});
 });
 
 module.exports = router;
